314f44b5efd75091cbe3b7b46d2317358d51b0e1
Ayan Ansari <corporate@stareng.co.in>
2026-04-13 20:28:31 +0530

    favicon

--- apps/admin-web/src/app/layout.tsx
+++ apps/admin-web/src/app/layout.tsx
@@ -1,15 +1,8 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "STAR ENGINEERING",
-  description: "STAR ENGINEERING PORTAL",
-    icons: {
-    icon: "/favicon.png",
-  }
-
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
