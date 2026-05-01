684155867d433d3f8e98ebfd6d9c39a13a55f3c8
Ayan Ansari <corporate@stareng.co.in>
2026-05-01 12:15:48 +0530

    Users Mobile

--- apps/admin-web/src/types/user.ts
+++ apps/admin-web/src/types/user.ts
@@ -27,10 +27,10 @@ export type CreateUserPayload = {
 export type UpdateUserPayload = {
   name: string;
   email: string;
-  phone?: string;
-  gstin?: string;
-  pan?: string;
-  address: string;
+  phone?: string | null;
+  gstin?: string | null;
+  pan?: string | null;
+  address?: string | null;
 };
 
 export type UsersResponse = {
